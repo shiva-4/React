686293a1c72e28e74365a8a9a4a2c0dafdc599fe
N shiva krishna <77113270+shiva-4@users.noreply.github.com>
2021-06-18 22:30:49 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>@copyrightt2022</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1>welcome to vbit</h1>
+      <p>
+        vbitt is an autonomous college well known fo its own clutural and other
+        clubs
+      </p>
+    </div>
+  );
+}
+export default Note;
